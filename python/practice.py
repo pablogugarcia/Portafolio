a43f5acffdd8e63c138637cb68921b94993d5554
PabloGarcia <pablo.garcia366@gmail.com>
2019-06-11 19:03:45 -0300

    update

---
+++ python/practice.py
@@ -0,0 +1,19 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+integer = 12
+double = 4.
+character = 'is the best place to learn and practice coding!'
+# Read and save an integer, double, and String to your variables.
+i += integer
+d += double
+s += character
+# Print the sum of both integer variables on a new line.
+print(i)
+print(d)
+print(s)
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
